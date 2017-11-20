03e1dbc44e0cd3002f47e13f45f7d6139b54c915
Ettiopgl <POGLIANI@rete.poste>
2017-11-20 23:07:12 +0100

    Versione con MsTimer2
    Unica striscia

--- Provapgl/src/main.cpp
+++ Provapgl/src/main.cpp
@@ -4,52 +4,49 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #include <Swimmer.h>
+#include <MsTimer2.h>
+#define MCD 10
 #define DEBUG 0
+#define TIMER1 1
 
-unsigned int recupero = 1000;
-unsigned int pausaSerie = 0;
-unsigned int pausaRip = 0;
+//volatile serve al compilatore per indicare che quella variabile viene gestita da una funzione "non standard".
+//variabile che dice se sono passati effettivamente 10ms
+volatile bool was_passed = false;
+//variabile che conta i decimi di millesimo trascorsi.
+volatile int cron = 0;
+
+//Funzione che viene chiamata ogni MCD millisecondi.
+void base_dei_tempi(){
+  cron++;
+  if (cron == TIMER1)
+    was_passed = true;
+}
 
 Adafruit_NeoPixel * strip = new Adafruit_NeoPixel(110, 30, NEO_GRB + NEO_KHZ800);
 
-void debug(String s, bool new_line = true, bool enable = DEBUG){
-  if (enable){
-    Serial.print(s+( (new_line) ? ("\n") : ("") ));
-  }
-}
-
 Swimmer s  (5, 0, 1, 255, 0, 0, 4, 2, 2);
-Swimmer s2 (5, 0, 1, 0, 255, 0, 4 ,2, 2);
-Swimmer s3 (5, 0, 1, 0, 0, 255, 4, 2, 2);
-unsigned long timer1, timer2, timer3, start1, start2, start3 ;
-bool is_terminated1 = false, is_terminated2 = false, is_terminated3 = false;
+//Swimmer s2 (5, 0, 1, 0, 255, 0, 4 ,2, 2);
+//Swimmer s3 (5, 0, 1, 0, 0, 255, 4, 2, 2);
 
 
 void setup(){
   Serial.begin(115200);
   Swimmer::setStrip(strip);
-  timer1 = millis();
-  timer2 = timer1;
-  timer3 = timer1;
-  start1 = millis();
-  start2 = start1;
-  start3 = start1;
-  recupero = 0;
-  pausaSerie = 0;
-  pausaRip = 0;
+  //Imposto i millesimi di secondo per chiamare base_dei_tempi.
+  MsTimer2::set(MCD, base_dei_tempi);
+  //Avvio la base dei tempi.
+  MsTimer2::start();
 }
 
 void loop(){
-  if (millis()-timer1 >5 && (millis() - start1) >= recupero && !is_terminated1){ // 20 è la velocità di scorrimento//
+  if (was_passed){
     s.autoStep(true);
-    timer1 = millis();
+    //Non sono passati più TIMER1 millisecondi.
+    was_passed = false;
+    cron = 0;
   }
   Swimmer::show();
-  //Singola serie finita.
   if(s.isFinishRip()){
     s.resetRip();
-    timer1 = millis();
-    start1 = timer1;
-    recupero = 5000;
   }
 }
